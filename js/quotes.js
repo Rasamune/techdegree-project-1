89a897e1e3a5fcecd635521b11872f4071a0863c
Christopher Jaeger <cjaeger.ld@gmail.com>
2020-11-24 21:40:07 -0500

    Multiple Changes (see Description)
    - Added Functions: printQuote, randomBackgroundColor
    - Added tags to quotes
    - Added Interval Timer

--- js/quotes.js
+++ js/quotes.js
@@ -3,78 +3,91 @@ const quotes = [
         quote: "Twenty years from now you will be more disappointed by the things that you didn’t do than by the ones you did do.",
         source: "H.Jackson Brown Jr.",
         citation: "P.S. I Love You",
-        year: "1990"
+        year: "1990",
+        tags: ["book", "movie"]
     },
     {
         quote: "It’s no use going back to yesterday, because I was a different person then.",
         source: "Lewis Carroll",
         citation: "Alice’s Adventures in Wonderland",
-        year: "1865"
+        year: "1865",
+        tags: ["book", "movie"]
     },
     {
         quote: "If we wait until we’re ready, we’ll be waiting for the rest of our lives.",
         source: "Lemony Snicket",
         citation: "The Ersatz Elevator",
-        year: "2001"
+        year: "2001",
+        tags: ["book", "movie"]
     },
     {
         quote: "The goal isn’t to live forever, the goal is to create something that will.",
         source: "Chuck Palahniuk",
         citation: "Diary",
-        year: "2003"
+        year: "2003",
+        tags: ["book"]
     },
     {
         quote: "None of us really changes over time. We only become more fully what we are.",
         source: "Anne Rice",
         citation: "The Vampire Lestat",
-        year: "1985"
+        year: "1985",
+        tags: ["book"]
     },
     {
         quote: "The only limits for tomorrow are the doubts we have today.",
         source: "Pittacus Lore",
         citation: "The Power of Six",
-        year: "2011"
+        year: "2011",
+        tags: ["book", "movie"]
     },
     {
         quote: "All endings are also beginnings. We just don’t know it at the time.",
         source: "Mitch Albom",
         citation: "The Five People You Meet In Heaven",
-        year: "2003"
+        year: "2003",
+        tags: ["book", "movie"]
     },
     {
         quote: "Yes: I am a dreamer. For a dreamer is one who can only find his way by moonlight, and his punishment is that he sees the dawn before the rest of the world.",
         source: "Oscar Wilde",
         citation: "The Critic as Artist",
-        year: "1891"
+        year: "1891",
+        tags: ["book"]
     },
     {
         quote: "If you’re making mistakes it means you’re out there doing something.",
         source: "Neil Gaiman",
         citation: "Make Good Art",
-        year: "2012"
+        year: "2012",
+        tags: ["book"]
     },
     {
         quote: "So many things are possible just as long as you don’t know they’re impossible.",
         source: "Norton Juster",
         citation: "The Phantom Tollbooth",
-        year: "1961"
+        year: "1961",
+        tags: ["book", "movie"]
     },
     {
         quote: "Time you enjoy wasting is not wasted time.",
         source: "Marthe Troly-Curtin",
         citation: "Phrynette Married",
-        year: "1911"
+        year: "1911",
+        tags: ["book"]
     },
     {
         quote: "Appear weak when you are strong, and strong when you are weak.",
         source: "Sun Tzu",
         citation: "The Art of War",
-        year: "5th century BC"
+        year: "5th century BC",
+        tags: ["book", "movie"]
     },
     {
         quote: "When you want something, all the universe conspires in helping you to achieve it.",
         source: "Paulo Coelho",
         citation: "The Alchemist",
-        year: "1988"
+        year: "1988",
+        tags: ["book", "movie"]
     }
   ];
--- js/script.js
+++ js/script.js
@@ -3,31 +3,75 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
-/*** 
- * `quotes` array 
-***/
-// Quotes can be found in js/quotes.js
-console.log(quotes);
+// --------------------------------------------------- //
+// Array of Quotes can be found in file 'js/quotes.js' //
+// --------------------------------------------------- //
 
-/***
- * `getRandomQuote` function
-***/
+// Set intervalTimer
+let intervalTimer;
+// Variable to check if quote is repeating
+let prevQuote = -1;
 
+// Generate a random number based on the number of quotes in the array
+function getRandomQuote () {
+  const randomNumber = Math.floor(Math.random() * quotes.length);
+  return quotes[randomNumber];
+}
 
+// Function to display a random quote and also update background color
+function printQuote() {
+  // Get random quote
+  let quote = getRandomQuote();
+  // If random quote is the same as the previous quote, get a new one
+  while(quote === prevQuote) {
+    quote = getRandomQuote();
+  }
 
-/***
- * `printQuote` function
-***/
+  // Create HTML to display the quote data
+  let html = `
+    <p class="quote">${quote.quote}</p>
+    <p class="source">${quote.source}
+  `;
+  if (quote.citation) { html += `<span class="citation">${quote.citation}</span>`; }
+  if (quote.year) { html += `<span class="citation">${quote.year}</span>`; }
+  if (quote.tags) {
+    html += `<br>`;
+    for ( let i = 0; i < quote.tags.length; i++ ){
+      html += `<span class="tags">/ ${quote.tags[i]} </span>`;
+    }
+  }
+  html += `</p>`;
 
+  // Update the quote in the html
+  document.getElementById('quote-box').innerHTML = html;
 
+  // Update background color
+  randomBackgroundColor();
 
-/***
- * click event listener for the print quote button
- * DO NOT CHANGE THE CODE BELOW!!
-***/
+  // Update timer and clear old timer
+  // (important when triggering the click event)
+  intervalTimer = clearInterval(intervalTimer);
+  intervalTimer = setInterval(printQuote, 10000);
 
+  // Set Previous Quote to current Quote
+  prevQuote = quote;
+}
+
+// Function to set a random background color
+function randomBackgroundColor() {
+  // Generate random color for the background
+  const red = Math.floor(Math.random() * 255);
+  const green = Math.floor(Math.random() * 255);
+  const blue = Math.floor(Math.random() * 255);
+  const rgb = `rgb(${red},${green},${blue})`;
+
+  // Update Background Color
+  document.body.style.backgroundColor = rgb;
+}
+
+// Get a random quote when the page loads
+printQuote();
+
+// Call printQuote when "Show Another Quote" is clicked
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
